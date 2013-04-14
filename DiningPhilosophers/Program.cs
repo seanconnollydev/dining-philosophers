3487e0f30ede3c426718e41df904af8ac3713286
Sean Connolly <sconnolly17@gmail.com>
2013-04-14 13:52:33 -0400

    Implementing correct implementation using monitors.

--- DiningPhilosophers/Program.cs
+++ DiningPhilosophers/Program.cs
@@ -1,4 +1,5 @@
-﻿// TODO: Compiler version and framework
+﻿// Framework used: .NET Framework 4
+// Compiler used: v4.0.30319.1
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,37 +10,52 @@ namespace DiningPhilosophers
 {
     class Program
     {
-        private const int PHILOSOPHER_COUNT = 5, TIMES_TO_EAT = 5;
+        private const int PHILOSOPHER_COUNT = 5;
 
         static void Main(string[] args)
         {
+            // Construct philosophers and chopsticks
             var philosophers = InitializePhilosophers();
 
+            // Start dinner
             Console.WriteLine("Dinner is starting!");
 
+            // Spawn threads for each philosopher's eating cycle
             var philosopherThreads = new List<Thread>();
             foreach (var philosopher in philosophers)
             {
                 var philosopherThread = new Thread(new ThreadStart(philosopher.EatAll));
                 philosopherThreads.Add(philosopherThread);
-
                 philosopherThread.Start();
             }
 
+            // Wait for all philosopher's to finish eating
             foreach (var thread in philosopherThreads)
             {
                 thread.Join();
             }
 
+            // Done
             Console.WriteLine("Dinner is over!");
         }
 
         private static List<Philosopher> InitializePhilosophers()
         {
+            // Construct philosophers
             var philosophers = new List<Philosopher>(PHILOSOPHER_COUNT);
             for (int i = 0; i < PHILOSOPHER_COUNT; i++)
             {
                 philosophers.Add(new Philosopher(philosophers, i));
+            }
+
+            // Assign chopsticks to each philosopher
+            foreach (var philosopher in philosophers)
+            {
+                // Assign left chopstick
+                philosopher.LeftChopstick = philosopher.LeftPhilosopher.RightChopstick ?? new Chopstick();
+
+                // Assign right chopstick
+                philosopher.RightChopstick = philosopher.RightPhilosopher.LeftChopstick ?? new Chopstick();
             }
 
             return philosophers;
@@ -53,6 +69,7 @@ namespace DiningPhilosophers
         private int _timesEaten = 0;
         private readonly List<Philosopher> _allPhilosophers;
         private readonly int _index;
+
         public Philosopher(List<Philosopher> allPhilosophers, int index)
         {
             _allPhilosophers = allPhilosophers;
@@ -62,8 +79,9 @@ namespace DiningPhilosophers
         }
 
         public string Name { get; private set; }
-
         public State State { get; private set; }
+        public Chopstick LeftChopstick { get; set; }
+        public Chopstick RightChopstick { get; set; }
 
         public Philosopher LeftPhilosopher
         {
@@ -93,43 +111,42 @@ namespace DiningPhilosophers
             while (_timesEaten < TIMES_TO_EAT)
             {
                 this.Think();
-                this.PickUp();
-                this.Eat();
-                this.PutDown();
+                if (this.PickUp())
+                {
+                    // Chopsticks acquired, eat up
+                    this.Eat();
+
+                    // Release chopsticks
+                    this.PutDownLeft();
+                    this.PutDownRight();
+                }
             }
         }
 
-        private void PickUp()
+        private bool PickUp()
         {
-            // If either left or right is eating, wait for chopsticks.
-            if (this.LeftPhilosopher.State == State.Eating || this.RightPhilosopher.State == State.Eating)
+            // Try to pick up the left chopstick
+            if (Monitor.TryEnter(this.LeftChopstick))
             {
-                Monitor.Wait(this);
-                this.State = State.Hungry;
+                Console.WriteLine(this.Name + " picks up left chopstick.");
+
+                // Now try to pick up the right
+                if (Monitor.TryEnter(this.RightChopstick))
+                {
+                    Console.WriteLine(this.Name + " picks up right chopstick.");
+
+                    // Both chopsticks acquired, its now time to eat
+                    return true;
+                }
+                else
+                {
+                    // Could not get the right chopstick, so put down the left
+                    this.PutDownLeft();
+                }
             }
 
-            Console.WriteLine(this.Name + " picks up left chopstick.");
-            Console.WriteLine(this.Name + " picks up right chopstick.");
-        }
-
-        private void PutDown()
-        {
-            // Give left neighbor a chance to eat.
-            if (this.LeftPhilosopher.State == State.Hungry &&
-                this.LeftPhilosopher.LeftPhilosopher.State != State.Eating)
-            {
-                Monitor.Pulse(this.LeftPhilosopher);
-            }
-
-            // Give right neighbor a chance to eat.
-            if (this.RightPhilosopher.State == State.Hungry &&
-                this.RightPhilosopher.RightPhilosopher.State != State.Eating)
-            {
-                Monitor.Pulse(this.RightPhilosopher);
-            }
-
-            Console.WriteLine(this.Name + " puts down left chopstick.");
-            Console.WriteLine(this.Name + " puts down right chopstick.");
+            // Could not acquire chopsticks, try again
+            return false;
         }
 
         private void Eat()
@@ -138,6 +155,19 @@ namespace DiningPhilosophers
             _timesEaten++;
             Console.WriteLine(this.Name + " eats.");
         }
+
+        private void PutDownLeft()
+        {
+            Monitor.Exit(this.LeftChopstick);
+            Console.WriteLine(this.Name + " puts down left chopstick.");
+        }
+
+        private void PutDownRight()
+        {
+            Monitor.Exit(this.RightChopstick);
+            Console.WriteLine(this.Name + " puts down right chopstick.");
+        }
+        
 
         private void Think()
         {
@@ -148,7 +178,18 @@ namespace DiningPhilosophers
     public enum State
     {
         Thinking = 0,
-        Hungry = 1,
-        Eating = 2
+        Eating = 1
+    }
+
+    [DebuggerDisplay("Name = {Name}")]
+    public class Chopstick
+    {
+        private static int _count = 1;
+        public string Name { get; private set; }
+
+        public Chopstick()
+        {
+            this.Name = "Chopstick " + _count++;
+        }
     }
 }
